6a3ce2ab7df1ef8cad4823b261b7cddf1c5a7439
pavel <geraci98.98@gmail.com>
2021-10-18 11:05:58 +0300

    add srollToTop and update style scroll in product-box

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Home from './Pages/Home';
 import ProductPage from './Pages/ProductPage';
 import { HashRouter, Route, Switch } from 'react-router-dom';
+import ScrollToTop from './Components/ScrollToTop/ScrollToTop'
 
 function App() {
 
   return (
     <HashRouter>
+      <ScrollToTop />
       <Switch>
         <Route path="/" exact component={Home}></Route>
         <Route path="/product" component={ProductPage}></Route>
